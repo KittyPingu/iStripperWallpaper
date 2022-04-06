a7c9e1e5740c0e21abc7293d77e47f854f51b795
Jeremy Palmer <jeremyp@outlook.com>
2022-04-06 14:31:46 +0100

    fix model names where there are more than 1 on a card

--- iStripperWallpaper/TrayContext.cs
+++ iStripperWallpaper/TrayContext.cs
@@ -180,31 +180,7 @@ namespace iStripperWallpaper
             ChangeWallpaper(true);
         }
 
-        public string ToPascalCase(string original)
-        {
-            Regex invalidCharsRgx = new Regex("[^_a-zA-Z0-9]");
-            Regex whiteSpace = new Regex(@"(?<=\s)");
-            Regex startsWithLowerCaseChar = new Regex("^[a-z]");
-            Regex firstCharFollowedByUpperCasesOnly = new Regex("(?<=[A-Z])[A-Z0-9]+$");
-            Regex lowerCaseNextToNumber = new Regex("(?<=[0-9])[a-z]");
-            Regex upperCaseInside = new Regex("(?<=[A-Z])[A-Z]+?((?=[A-Z][a-z])|(?=[0-9]))");
-
-            // replace white spaces with undescore, then replace all invalid chars with empty string
-            var pascalCase = invalidCharsRgx.Replace(whiteSpace.Replace(original, "_"), string.Empty)
-                // split by underscores
-                .Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries)
-                // set first letter to uppercase
-                .Select(w => startsWithLowerCaseChar.Replace(w, m => m.Value.ToUpper()))
-                // replace second and all following upper case letters to lower if there is no next lower (ABC -> Abc)
-                .Select(w => firstCharFollowedByUpperCasesOnly.Replace(w, m => m.Value.ToLower()))
-                // set upper case the first lower case following a number (Ab9cd -> Ab9Cd)
-                .Select(w => lowerCaseNextToNumber.Replace(w, m => m.Value.ToUpper()))
-                // lower second and next upper case letters except the last if it follows by any lower (ABcDEf -> AbcDef)
-                .Select(w => upperCaseInside.Replace(w, m => m.Value.ToLower()));
-
-            return string.Concat(pascalCase);
-        }
-
+    
         public static string PascalCase(string word)
         {
             return string.Join(" " , word.Split('_')
@@ -253,10 +229,19 @@ namespace iStripperWallpaper
             {
                 nowPlayingTagShort = nowplaying.Split("\\")[0];
                 var c = StaticPropertiesLoader.getCardByID(nowPlayingTagShort);
-                var m = StaticPropertiesLoader.getModelByID(c.modelID);
-                var d = StaticPropertiesLoader.getBioByID(c.modelID);
                 outfit = c.name;
-                model = PascalCase(d.Name);
+                var modelIDs = c.modelID.Split(",");
+                string modelName = "";
+                foreach(var cm in modelIDs)
+                { 
+                    //var m = StaticPropertiesLoader.getModelByID(cm);
+                    var d = StaticPropertiesLoader.getBioByID(cm);
+                    if (modelName != "")
+                        modelName += " &_" + d.Name;
+                    else
+                        modelName = d.Name;
+                }
+                model = PascalCase(modelName);
             }
         }
 
